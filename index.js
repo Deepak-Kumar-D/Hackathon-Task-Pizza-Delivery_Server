b3c923c67a7d8ebdbcac3f2fedf39224657f5ed8
Deepak-Kumar-D <deepak.kumard6@gmail.com>
2021-07-11 21:29:35 +0530

    Cookie added

---
+++ index.js
@@ -0,0 +1,99 @@
+import express from "express";
+import mongoose from "mongoose";
+import { User } from "./models/pizzaTown.js";
+import cors from "cors";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+
+var corsOptions = {
+  origin: true,
+  credentials: true  
+}
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const url = process.env.MONGODB_URI || "mongodb://localhost/pizzaTown";
+app.use(cors(corsOptions));
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const con = mongoose.connection;
+con.on("open", () => console.log("MongoDB is connected!"));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (request, response) => {
+  response.send("Welcome to node app!");
+});
+
+// Find users
+app.get("/users", async (request, response) => {
+  const users = await User.find();
+
+  response.send(users);
+});
+
+//Add a user
+app.post("/register", async (request, response) => {
+  const { name, email, phone, address, password } = request.body;
+
+  if ((!name, !email, !phone, !address, !password)) {
+    return response
+      .status(422)
+      .json({ error: "Please leave the field empty!" });
+  }
+
+  try {
+    const userExist = await User.findOne({ email: email });
+
+    if (userExist) {
+      return response.status(422).json({ error: "Email-Id already exists!" });
+    }
+
+    const user = new User({ name, email, phone, address, password });
+    await user.save();
+
+    response.status(201).json({ error: "User Registered Successfully!" });
+  } catch (err) {
+    response.send(err);
+  }
+});
+
+//Login
+app.post("/login", async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    if (!email || !password) {
+      return response.status(400).json({ error: "Please fill the fields!" });
+    }
+
+    const userLogin = await User.findOne({ email: email });
+
+    if (userLogin) {
+      const isMatch = await bcrypt.compare(password, userLogin.password);
+
+      const token = await userLogin.generateAuthToken();
+
+      response.cookie("jwttoken", token, {
+        expires: new Date(Date.now() + 2592000000),
+        httpOnly: true,
+      });
+
+      if (!isMatch) {
+        response.status(400).json({ error: "Invalid Credentials!" });
+      } else {
+        response.json({ message: "User Login Success!" })
+      }
+    } else {
+      response.status(400).json({ error: "Invalid Credentials!" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("The server is started in PORT " + PORT + "...");
+});
---
+++ models/pizzaTown.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: Number, required: true },
+  address: { type: String, required: true },
+  password: { type: String, required: true },
+  tokens: [{ token: { type: String, required: true } }],
+});
+
+//Hashing Password
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+});
+
+//Token Generation
+userSchema.methods.generateAuthToken = async function () {
+  try {
+    let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+    this.tokens = this.tokens.concat({ token: token });
+    await this.save();
+    return token;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const User = mongoose.model("User", userSchema);
