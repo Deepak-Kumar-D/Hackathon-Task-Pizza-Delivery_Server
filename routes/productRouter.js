97b8697743e6633c12aef4bbaf2519d940b12520
Deepak Kumar D <deepak.kumard36@gmail.com>
2021-09-20 20:50:39 +0530

    stock update for pizza and mail update is added

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,6 +3,7 @@ import { Product } from "../models/product.js";
 import { Admin } from "../models/admin.js";
 import { User } from "../models/users.js";
 import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
 
 const productRouter = express.Router();
 
@@ -44,19 +45,75 @@ productRouter.get("/user-products", async (request, response) => {
   }
 });
 
-productRouter.post("/add-products", async (request, response) => {
-  const { name, src, price, quantity } = request.body;
+// productRouter.post("/add-products", async (request, response) => {
+//   const { name, src, price, quantity } = request.body;
 
-  const product = await Product.findOne({ name: name });
+//   const product = await Product.findOne({ name: name });
 
-  if (product) {
-    return response.status(400).json("Product already exists.");
+//   if (product) {
+//     return response.status(400).json("Product already exists.");
+//   }
+
+//   const newProduct = new Product({ name, src, price, quantity });
+//   newProduct.save();
+
+//   response.status(200).json("Product added successfully.");
+// });
+
+productRouter.post("/remove-quantity", async (request, response) => {
+  const { items } = request.body;
+
+  {
+    items.forEach(async (ele) => {
+      const product = await Product.findOne({ _id: ele.productId });
+      product.quantity = product.quantity - ele.quantity;
+      await product.save();
+
+      if (product.quantity < 10) {
+        const sendMail = () => {
+          let Transport = nodemailer.createTransport({
+            service: "Gmail",
+            auth: {
+              user: process.env.MAIL_USERNAME,
+              pass: process.env.MAIL_PASSWORD,
+            },
+          });
+
+          let mailOptions = {
+            from: `"PIZZA TOWN"` + "<" + process.env.MAIL_USERNAME + ">",
+            to: { name: "Automated", address: process.env.MAIL_USERNAME },
+            subject: "Stock Update",
+            html: `<p>Dear Admin,</p>\n
+            <h3>The stock for ${product.name} is very low. Please update the stocks.</h3>\n
+            <p>Regards,</p>\n
+            <p>Pizza Town</p>\n
+            <p>India</p>`,
+          };
+
+          Transport.sendMail(mailOptions, function (error, response) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log("Stock maintainance mail sent!");
+            }
+          });
+        };
+
+        sendMail();
+      }
+    });
   }
+  response.json("Product list updated.");
+});
 
-  const newProduct = new Product({ name, src, price, quantity });
-  newProduct.save();
+productRouter.post("/add-quantity", async (request, response) => {
+  const { id, qty } = request.body;
 
-  response.status(200).json("Product added successfully.");
+  const product = await Product.findOne({ _id: id });
+  product.quantity = qty;
+  await product.save();
+
+  response.json("Stock updated!");
 });
 
 export { productRouter };
